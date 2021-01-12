6472d3e75ab0daecce67674ba264da841600962a
Filip van Laenen <f.a.vanlaenen@ieee.org>
2021-01-12 07:30:09 +0100

    Issue #45: Added unit tests to OrProducer

---
+++ src/test/java/net/filipvanlaenen/iacaj/producer/OrProducerTest.java
@@ -0,0 +1,35 @@
+package net.filipvanlaenen.iacaj.producer;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.Arrays;
+
+import org.junit.jupiter.api.Test;
+
+import net.filipvanlaenen.iacaj.BooleanFunction;
+
+public class OrProducerTest {
+    @Test
+    public void shouldProduceOrFunctionWithWordLength1() {
+        Producer producer = new OrProducer(Arrays.asList(new Integer[] {1}));
+        BooleanFunction booleanFunction = producer.produce();
+        StringBuilder sb = new StringBuilder();
+        sb.append("v1 = i1 ∨ i2\n");
+        sb.append("o1 = v1");
+        String expected = sb.toString();
+        assertEquals(expected, booleanFunction.toString());
+    }
+
+    @Test
+    public void shouldProduceOrFunctionWithWordLength2() {
+        Producer producer = new OrProducer(Arrays.asList(new Integer[] {2}));
+        BooleanFunction booleanFunction = producer.produce();
+        StringBuilder sb = new StringBuilder();
+        sb.append("v1 = i1 ∨ i3\n");
+        sb.append("v2 = i2 ∨ i4\n");
+        sb.append("o1 = v1\n");
+        sb.append("o2 = v2");
+        String expected = sb.toString();
+        assertEquals(expected, booleanFunction.toString());
+    }
+}
