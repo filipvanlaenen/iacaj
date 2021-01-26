38a8eac851a944ade153b9558ce3b06070407e0d
Filip van Laenen <f.a.vanlaenen@ieee.org>
2021-01-26 11:57:54 +0100

    Issue #52: Continued work on CollisionFound

--- src/main/java/net/filipvanlaenen/iacaj/CollisionFound.java
+++ src/main/java/net/filipvanlaenen/iacaj/CollisionFound.java
@@ -1,5 +1,9 @@
 package net.filipvanlaenen.iacaj;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
@@ -11,6 +15,18 @@ public class CollisionFound implements AttackResult {
 	public CollisionFound(BooleanFunction collission, Set<InputParameter> inputParameters) {
 		this.collision = collission;
 		this.inputParameters = inputParameters;
+	}
+
+	private String asSortedInputParameterNamesString(Set<InputParameter> inputParameterSet) {
+		List<InputParameter> sortedInputParameters = new ArrayList<InputParameter>(inputParameterSet);
+		Collections.sort(sortedInputParameters, new Comparator<InputParameter>() {
+			@Override
+			public int compare(InputParameter arg0, InputParameter arg1) {
+				return arg0.getNumber() - arg1.getNumber();
+			}
+		});
+		List<String> names = sortedInputParameters.stream().map(InputParameter::getName).collect(Collectors.toList());
+		return String.join(", ", names);
 	}
 
 	@Override
@@ -28,30 +44,28 @@ public class CollisionFound implements AttackResult {
 				.collect(Collectors.toList());
 		sb.append(String.join("\n    ", constraintLines));
 		sb.append("\n");
-		Set<String> eliminatedInputParameterNames = inputParameters.stream().map(InputParameter::getName)
-				.collect(Collectors.toSet());
-		Set<String> constrainedInputParameterNames = constraints.stream().map(BooleanConstraint::getName)
-				.collect(Collectors.toSet());
-		eliminatedInputParameterNames.removeAll(constrainedInputParameterNames);
-		Set<String> freeInputParameterNames = collision.getInputParameters().stream().map(InputParameter::getName)
-				.collect(Collectors.toSet());
-		eliminatedInputParameterNames.removeAll(freeInputParameterNames);
-		if (eliminatedInputParameterNames.size() == 1) {
+		Set<InputParameter> eliminatedInputParameters = new HashSet<InputParameter>(inputParameters);
+		Set<InputParameter> constrainedInputParameterNames = constraints.stream()
+				.map(c -> InputParameter.get(c.getName())).collect(Collectors.toSet());
+		eliminatedInputParameters.removeAll(constrainedInputParameterNames);
+		Set<InputParameter> freeInputParameters = collision.getInputParameters();
+		eliminatedInputParameters.removeAll(freeInputParameters);
+		if (eliminatedInputParameters.size() == 1) {
 			sb.append("  Eliminated input parameter: ");
 		} else {
 			sb.append("  Eliminated input parameters: ");
 		}
-		sb.append(String.join(", ", eliminatedInputParameterNames));
+		sb.append(asSortedInputParameterNamesString(eliminatedInputParameters));
 		sb.append("\n");
-		if (freeInputParameterNames.size() == 1) {
+		if (freeInputParameters.size() == 1) {
 			sb.append("  Free input parameter: ");
 		} else {
 			sb.append("  Free input parameters: ");
 		}
-		if (freeInputParameterNames.isEmpty()) {
+		if (freeInputParameters.isEmpty()) {
 			sb.append("none");
 		} else {
-			sb.append(String.join(", ", freeInputParameterNames));
+			sb.append(asSortedInputParameterNamesString(freeInputParameters));
 		}
 		return sb.toString();
 	}
--- src/main/java/net/filipvanlaenen/iacaj/Variable.java
+++ src/main/java/net/filipvanlaenen/iacaj/Variable.java
@@ -4,26 +4,32 @@ package net.filipvanlaenen.iacaj;
  * Class representing a variable.
  */
 public class Variable {
-    /**
-     * The name of the variable.
-     */
-    private final String name;
+	/**
+	 * The name of the variable.
+	 */
+	private final String name;
+	private final int number;
 
-    /**
-     * Constructor with the variable's name as parameter.
-     *
-     * @param name The name of the variable.
-     */
-    protected Variable(final String name) {
-        this.name = name;
-    }
+	/**
+	 * Constructor with the variable's name as parameter.
+	 *
+	 * @param name The name of the variable.
+	 */
+	protected Variable(final String name) {
+		this.name = name;
+		this.number = Integer.parseInt(name.substring(1));
+	}
 
-    /**
-     * Returns the name of the variable.
-     *
-     * @return The name of the variable.
-     */
-    public String getName() {
-        return name;
-    }
+	/**
+	 * Returns the name of the variable.
+	 *
+	 * @return The name of the variable.
+	 */
+	public String getName() {
+		return name;
+	}
+
+	public int getNumber() {
+		return number;
+	}
 }
