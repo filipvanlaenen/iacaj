8c271fa3bff51ef7e23bda71de6b1ae5906b96a4
Filip van Laenen <f.a.vanlaenen@ieee.org>
2021-02-05 07:57:55 +0100

    Issue #52: Resolved Checkstyle violations in NoCollisionFoundYet

--- src/main/java/net/filipvanlaenen/iacaj/NoCollisionFoundYet.java
+++ src/main/java/net/filipvanlaenen/iacaj/NoCollisionFoundYet.java
@@ -1,9 +1,20 @@
 package net.filipvanlaenen.iacaj;
 
-public class NoCollisionFoundYet implements AttackResult {
-
+/**
+ * Attack result when no collision was found yet in a Boolean function after a
+ * limited number of iterations.
+ */
+public final class NoCollisionFoundYet implements AttackResult {
+    /**
+     * The number of iterations done.
+     */
     private final int numberOfIterations;
 
+    /**
+     * Constructor taking the number of iterations as a parameter.
+     *
+     * @param numberOfIterations The number of iterations done.
+     */
     public NoCollisionFoundYet(final int numberOfIterations) {
         this.numberOfIterations = numberOfIterations;
     }
