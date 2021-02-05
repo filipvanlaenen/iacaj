72752791a3b4caa9a7d20c567b9c3c6c70163200
Filip van Laenen <f.a.vanlaenen@ieee.org>
2021-02-05 07:53:08 +0100

    Issue #52: Resolved Checkstyle violations in Variable

--- src/main/java/net/filipvanlaenen/iacaj/Variable.java
+++ src/main/java/net/filipvanlaenen/iacaj/Variable.java
@@ -4,32 +4,40 @@ package net.filipvanlaenen.iacaj;
  * Class representing a variable.
  */
 public class Variable {
-	/**
-	 * The name of the variable.
-	 */
-	private final String name;
-	private final int number;
+    /**
+     * The name of the variable.
+     */
+    private final String name;
+    /**
+     * The number of the variable.
+     */
+    private final int number;
 
-	/**
-	 * Constructor with the variable's name as parameter.
-	 *
-	 * @param name The name of the variable.
-	 */
-	protected Variable(final String name) {
-		this.name = name;
-		this.number = Integer.parseInt(name.substring(1));
-	}
+    /**
+     * Constructor with the variable's name as parameter.
+     *
+     * @param name The name of the variable.
+     */
+    protected Variable(final String name) {
+        this.name = name;
+        this.number = Integer.parseInt(name.substring(1));
+    }
 
-	/**
-	 * Returns the name of the variable.
-	 *
-	 * @return The name of the variable.
-	 */
-	public String getName() {
-		return name;
-	}
+    /**
+     * Returns the name of the variable.
+     *
+     * @return The name of the variable.
+     */
+    public String getName() {
+        return name;
+    }
 
-	public int getNumber() {
-		return number;
-	}
+    /**
+     * Returns the number of the variable.
+     *
+     * @return The number of the variable.
+     */
+    public int getNumber() {
+        return number;
+    }
 }
