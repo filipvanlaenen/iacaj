68a076bdce55327c3c622ca36b563e0a23609ba0
Filip van Laenen <f.a.vanlaenen@ieee.org>
2021-02-05 07:55:04 +0100

    Issue #52: Resolved Checkstyle violations in SomeInputParametersEliminated

--- src/main/java/net/filipvanlaenen/iacaj/SomeInputParametersEliminated.java
+++ src/main/java/net/filipvanlaenen/iacaj/SomeInputParametersEliminated.java
@@ -1,5 +1,9 @@
 package net.filipvanlaenen.iacaj;
 
+/**
+ * Attack result when some input parameters were eliminated after resolving the
+ * original Boolean function.
+ */
 public class SomeInputParametersEliminated implements AttackResult {
 
 }
