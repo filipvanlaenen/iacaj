1823510e2864ec9118df371ebc25a102824f6e58
Filip van Laenen <f.a.vanlaenen@ieee.org>
2021-02-05 07:58:57 +0100

    Issue #52: Resolved Checkstyle violations in NoCollisionFound

--- src/main/java/net/filipvanlaenen/iacaj/NoCollisionFound.java
+++ src/main/java/net/filipvanlaenen/iacaj/NoCollisionFound.java
@@ -3,7 +3,7 @@ package net.filipvanlaenen.iacaj;
 /**
  * Attack result when no collision could be found in the Boolean function.
  */
-public class NoCollisionFound implements AttackResult {
+public final class NoCollisionFound implements AttackResult {
     @Override
     public String toString() {
         return "No collision found in the Boolean function.";
