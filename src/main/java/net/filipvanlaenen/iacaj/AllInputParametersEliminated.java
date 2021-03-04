358a7bfbc4b610ad0fd76a522d29a603808c144d
Filip van Laenen <f.a.vanlaenen@ieee.org>
2021-03-04 07:34:46 +0100

    Issue #52: Resolved a Checkstyle violation

--- src/main/java/net/filipvanlaenen/iacaj/AllInputParametersEliminated.java
+++ src/main/java/net/filipvanlaenen/iacaj/AllInputParametersEliminated.java
@@ -4,7 +4,7 @@ package net.filipvanlaenen.iacaj;
  * Attack result when all input parameters are eliminated after the initial
  * resolution of the Boolean function.
  */
-public class AllInputParametersEliminated implements AttackResult {
+public final class AllInputParametersEliminated implements AttackResult {
     @Override
     public String toString() {
         return "All input parameters were eliminated while resolving the Boolean function.";
