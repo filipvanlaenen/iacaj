d69fb8cc52c766c131f6278ae7682d3cfbf93fbb
Filip van Laenen <f.a.vanlaenen@ieee.org>
2021-05-14 10:21:33 +0200

    Issue #62: Added unit tests for AttackRecord

--- src/test/java/net/filipvanlaenen/iacaj/AttackRecordTest.java
+++ src/test/java/net/filipvanlaenen/iacaj/AttackRecordTest.java
@@ -15,6 +15,23 @@ import org.junit.jupiter.api.Test;
  */
 public class AttackRecordTest {
     /**
+     * Input parameter i1.
+     */
+    private static final InputParameter INPUT_PARAMETER_1 = InputParameter.get("i1");
+    /**
+     * Input parameter i2.
+     */
+    private static final InputParameter INPUT_PARAMETER_2 = InputParameter.get("i2");
+    /**
+     * Input parameter i3.
+     */
+    private static final InputParameter INPUT_PARAMETER_3 = InputParameter.get("i3");
+    /**
+     * Input parameter i4.
+     */
+    private static final InputParameter INPUT_PARAMETER_4 = InputParameter.get("i4");
+
+    /**
      * Verifies that the constructor extracts the input parameters from the Boolean
      * function.
      */
@@ -24,8 +41,8 @@ public class AttackRecordTest {
         BooleanFunction booleanFunction = BooleanFunction.parse(content);
         AttackRecord attackRecord = new AttackRecord(booleanFunction);
         Set<InputParameter> expected = new HashSet<InputParameter>();
-        expected.add(InputParameter.get("i1"));
-        expected.add(InputParameter.get("i2"));
+        expected.add(INPUT_PARAMETER_1);
+        expected.add(INPUT_PARAMETER_2);
         assertEquals(expected, attackRecord.getInputParameters());
     }
 
@@ -64,13 +81,32 @@ public class AttackRecordTest {
      */
     @Test
     public void inputParametersArePrioritizedAccordingToTheirNumberOfOccurrences() {
-        String[] content = new String[] {"o1 = i1 ∧ i2 ∧ i3", "o2 = i2 ∧ i3", "o2 = i3"};
+        String[] content = new String[] {"o1 = i1 ∧ i2 ∧ i3 ∧ i4", "o2 = i2 ∧ i3 ∧ i4", "o2 = i3 ∧ i4", "o3 = i4"};
         BooleanFunction booleanFunction = BooleanFunction.parse(content);
         AttackRecord attackRecord = new AttackRecord(booleanFunction);
         List<InputParameter> expected = new ArrayList<InputParameter>();
-        expected.add(InputParameter.get("i3"));
-        expected.add(InputParameter.get("i2"));
-        expected.add(InputParameter.get("i1"));
+        expected.add(INPUT_PARAMETER_4);
+        expected.add(INPUT_PARAMETER_3);
+        expected.add(INPUT_PARAMETER_2);
+        expected.add(INPUT_PARAMETER_1);
         assertEquals(expected, attackRecord.getPrioritizedInputParameters());
+    }
+
+    /**
+     * Verifies that the input parameter pairs are prioritized according to their
+     * number of occurrences.
+     */
+    @Test
+    public void inputParameterPairsArePrioritizedAccordingToTheirNumberOfOccurrences() {
+        String[] content = new String[] {"o1 = i1 ∧ i2 ∧ i3", "o2 = i1 ∧ i3", "o2 = i1 ∧ i2", "o3 = i1 ∨ i2",
+                "o4 = i1 ∧ i4", "o1 = i1 ∨ i2 ∨ i3"};
+        BooleanFunction booleanFunction = BooleanFunction.parse(content);
+        AttackRecord attackRecord = new AttackRecord(booleanFunction);
+        List<InputParameterPair> expected = new ArrayList<InputParameterPair>();
+        expected.add(new InputParameterPair(INPUT_PARAMETER_1, INPUT_PARAMETER_2));
+        expected.add(new InputParameterPair(INPUT_PARAMETER_1, INPUT_PARAMETER_3));
+        expected.add(new InputParameterPair(INPUT_PARAMETER_2, INPUT_PARAMETER_3));
+        expected.add(new InputParameterPair(INPUT_PARAMETER_1, INPUT_PARAMETER_4));
+        assertEquals(expected, attackRecord.getPrioritizedInputParameterPairs());
     }
 }
