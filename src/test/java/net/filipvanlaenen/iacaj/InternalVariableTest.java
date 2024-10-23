80b96b7f20a93de0d155b309d0a7cb6d5384bdfa
Filip Van Laenen <f.a.vanlaenen@ieee.org>
2024-10-23 14:30:14 +0200

    Added unit tests to InternalVariableTest

--- src/test/java/net/filipvanlaenen/iacaj/InternalVariableTest.java
+++ src/test/java/net/filipvanlaenen/iacaj/InternalVariableTest.java
@@ -1,6 +1,8 @@
 package net.filipvanlaenen.iacaj;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
 
@@ -9,8 +11,8 @@ import org.junit.jupiter.api.Test;
  */
 public class InternalVariableTest {
     /**
-     * Verifies that an internal variable can be retrieved. The name used has a high
-     * number to increase the chance that it hasn't een used before.
+     * Verifies that an internal variable can be retrieved. The name used has a high number to increase the chance that
+     * it hasn't een used before.
      */
     @Test
     public void getShouldReturnAnInternalVariableWithTheCorrectName() {
@@ -18,13 +20,36 @@ public class InternalVariableTest {
     }
 
     /**
-     * Verifies that the same internal variable is retrieved when using the same
-     * name. The name used has a high number to increase the chance that it hasn't
-     * een used before.
+     * Verifies that the same internal variable is retrieved when using the same name. The name used has a high number
+     * to increase the chance that it hasn't een used before.
      */
     @Test
     public void getShouldReturnTheSameInternalVariableForTheSameName() {
         InternalVariable expected = InternalVariable.get("v100002");
         assertEquals(expected, InternalVariable.get("v100002"));
+    }
+
+    /**
+     * Verifies that a parameter starting with an i is not recognized as an internal variable.
+     */
+    @Test
+    public void isInternalVariableReturnsTrueWhenStartingWithI() {
+        assertFalse(InternalVariable.isInternalVariable("i1"));
+    }
+
+    /**
+     * Verifies that a parameter starting with a v is recognized as an internal variable.
+     */
+    @Test
+    public void isInternalVariableReturnsFalseWhenStartingWithV() {
+        assertTrue(InternalVariable.isInternalVariable("v1"));
+    }
+
+    /**
+     * Verifies that a parameter starting with an o is not recognized as an internal variable.
+     */
+    @Test
+    public void isInternalVariableReturnsFalseWhenStartingWithO() {
+        assertFalse(InternalVariable.isInternalVariable("o1"));
     }
 }
