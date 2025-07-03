ae48b86b03c682ddeefb7ee358b10fd280ecbf88
Filip Van Laenen <f.a.vanlaenen@ieee.org>
2025-07-03 15:27:46 +0200

    Issue #80: Replaced Java collections with Kolektoj collections

--- src/test/java/net/filipvanlaenen/iacaj/AttackRecordTest.java
+++ src/test/java/net/filipvanlaenen/iacaj/AttackRecordTest.java
@@ -4,9 +4,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import java.util.ArrayList;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
 
 import org.junit.jupiter.api.Test;
