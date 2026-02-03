dc797d4b5c0b5a0aa2a99546c0af7bd9c0e17e3b
Filip Van Laenen <f.a.vanlaenen@ieee.org>
2026-02-03 16:46:23 +0100

    Pulled up generalized functionality for the rotation

--- src/main/java/net/filipvanlaenen/iacaj/builders/RotationFunctionBuilder.java
+++ src/main/java/net/filipvanlaenen/iacaj/builders/RotationFunctionBuilder.java
@@ -1,32 +1,12 @@
 package net.filipvanlaenen.iacaj.builders;
 
 import net.filipvanlaenen.iacaj.expressions.Expression;
-import net.filipvanlaenen.iacaj.expressions.IdentityExpression;
 import net.filipvanlaenen.iacaj.expressions.Variable;
 import net.filipvanlaenen.iacaj.expressions.VectorialFunction;
-import net.filipvanlaenen.kolektoj.Map;
 import net.filipvanlaenen.kolektoj.ModifiableMap;
-import net.filipvanlaenen.kolektoj.OrderedCollection;
 
 public final class RotationFunctionBuilder extends VariableWidthVectorialFunctionBuilder {
-    private class Word {
-        private final OrderedCollection<Variable> variables;
-
-        Word(String inputVectorName, Integer outputVectorWidth) {
-            variables =
-                    OrderedCollection.createSequence(i -> new Variable(inputVectorName + (i + 1)), outputVectorWidth);
-        }
-
-        Variable getAt(int i) {
-            return variables.getAt(i);
-        }
-
-        int size() {
-            return variables.size();
-        }
-    }
-
-    private Integer rotateRight;
+    Integer rotateRight;
 
     @Override
     public VectorialFunction build() throws IllegalStateException {
@@ -40,27 +20,11 @@ public final class RotationFunctionBuilder extends VariableWidthVectorialFunctio
         if (rotateRight == null) {
             throw new IllegalStateException("Cannot build a rotate function when the rotate right isn't set.");
         }
-        ModifiableMap<Variable, Expression> map = ModifiableMap.empty();
         Word inputVector = new Word(inputVectorName, outputVectorWidth);
         Word outputVector = new Word(outputVectorName, outputVectorWidth);
-        map.addAll(createRotation(inputVector, outputVector));
-        return new VectorialFunction(map);
-    }
-
-    private Map<Variable, Expression> createRotation(Word inputVector, Word outputVector) {
-        int width = outputVector.size();
-        if (width != inputVector.size()) {
-            throw new IllegalStateException(
-                    "Input and output vector should have the same width for a rotation operation.");
-        }
         ModifiableMap<Variable, Expression> map = ModifiableMap.empty();
-        for (int i = 0; i < width; i++) {
-            Variable ivi = inputVector.getAt(i);
-            int j = (i + rotateRight) % width;
-            Variable ovi = outputVector.getAt(j);
-            map.add(ovi, new IdentityExpression(ivi));
-        }
-        return map;
+        map.addAll(createRotation(inputVector, outputVector, rotateRight));
+        return new VectorialFunction(map);
     }
 
     public void rotateRight(Integer rotateRight) {
--- src/main/java/net/filipvanlaenen/iacaj/builders/VectorialFunctionBuilder.java
+++ src/main/java/net/filipvanlaenen/iacaj/builders/VectorialFunctionBuilder.java
@@ -1,12 +1,51 @@
 package net.filipvanlaenen.iacaj.builders;
 
+import net.filipvanlaenen.iacaj.expressions.Expression;
+import net.filipvanlaenen.iacaj.expressions.IdentityExpression;
+import net.filipvanlaenen.iacaj.expressions.Variable;
 import net.filipvanlaenen.iacaj.expressions.VectorialFunction;
+import net.filipvanlaenen.kolektoj.Map;
+import net.filipvanlaenen.kolektoj.ModifiableMap;
+import net.filipvanlaenen.kolektoj.OrderedCollection;
 
 public abstract class VectorialFunctionBuilder {
+    protected class Word {
+        private final OrderedCollection<Variable> variables;
+
+        Word(String inputVectorName, Integer outputVectorWidth) {
+            variables =
+                    OrderedCollection.createSequence(i -> new Variable(inputVectorName + (i + 1)), outputVectorWidth);
+        }
+
+        Variable getAt(int i) {
+            return variables.getAt(i);
+        }
+
+        int size() {
+            return variables.size();
+        }
+    }
+
     private String inputVectorName = "x";
     private String outputVectorName = "y";
 
     abstract public VectorialFunction build() throws IllegalStateException;
+
+    protected Map<Variable, Expression> createRotation(Word inputVector, Word outputVector, int rotateRight) {
+        int width = outputVector.size();
+        if (width != inputVector.size()) {
+            throw new IllegalStateException(
+                    "Input and output vector should have the same width for a rotation operation.");
+        }
+        ModifiableMap<Variable, Expression> map = ModifiableMap.empty();
+        for (int i = 0; i < width; i++) {
+            Variable ivi = inputVector.getAt(i);
+            int j = (i + rotateRight) % width;
+            Variable ovi = outputVector.getAt(j);
+            map.add(ovi, new IdentityExpression(ivi));
+        }
+        return map;
+    }
 
     protected String getInputVectorName() {
         return inputVectorName;
