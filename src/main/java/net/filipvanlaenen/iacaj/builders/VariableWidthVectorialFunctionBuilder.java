b1efd5c56dabb7d9c680af3b5e7be54d1238dc01
Filip Van Laenen <f.a.vanlaenen@ieee.org>
2026-02-13 15:04:26 +0100

    Resolved Checkstyle violations in VariableWidthVectorialFunctionBuilder

--- src/main/java/net/filipvanlaenen/iacaj/builders/VariableWidthVectorialFunctionBuilder.java
+++ src/main/java/net/filipvanlaenen/iacaj/builders/VariableWidthVectorialFunctionBuilder.java
@@ -1,16 +1,37 @@
 package net.filipvanlaenen.iacaj.builders;
 
+/**
+ * Abstract class for builders with a variable output vector width.
+ */
 abstract class VariableWidthVectorialFunctionBuilder extends VectorialFunctionBuilder {
+    /**
+     * The output vector width.
+     */
     private Integer outputVectorWidth;
 
+    /**
+     * Returns the output vector width.
+     *
+     * @return The output vector width.
+     */
     protected Integer getOutputVectorWidth() {
         return outputVectorWidth;
     }
 
-    public void outputVectorWidth(final Integer width) {
-        this.outputVectorWidth = width;
+    /**
+     * Sets the output vector width.
+     *
+     * @param newOutputVectorWidth The output vector width.
+     */
+    public void outputVectorWidth(final Integer newOutputVectorWidth) {
+        this.outputVectorWidth = newOutputVectorWidth;
     }
 
+    /**
+     * Runs assertions before the build phase is performed, typically verifying the state of the builder.
+     *
+     * @throws IllegalStateException Thrown if the builder isn't in a state ready to build.
+     */
     protected void prebuild() throws IllegalStateException {
         super.prebuild();
         if (outputVectorWidth == null) {
