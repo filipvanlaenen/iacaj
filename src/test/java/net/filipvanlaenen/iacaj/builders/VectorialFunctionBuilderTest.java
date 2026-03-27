7976fcf0a5c6864e6b80febd5108dedd55d25a02
Filip Van Laenen <f.a.vanlaenen@ieee.org>
2026-03-27 12:55:57 +0100

    Added unit tests to VectorialFunctionBuilder

--- src/test/java/net/filipvanlaenen/iacaj/builders/VectorialFunctionBuilderTest.java
+++ src/test/java/net/filipvanlaenen/iacaj/builders/VectorialFunctionBuilderTest.java
@@ -5,6 +5,7 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.Test;
 
 import net.filipvanlaenen.iacaj.expressions.Expression;
+import net.filipvanlaenen.iacaj.expressions.Operator;
 import net.filipvanlaenen.iacaj.expressions.Variable;
 import net.filipvanlaenen.iacaj.expressions.VectorialFunction;
 import net.filipvanlaenen.iacaj.expressions.Word;
@@ -15,25 +16,88 @@ import net.filipvanlaenen.kolektoj.ModifiableMap;
  */
 public class VectorialFunctionBuilderTest {
     /**
-     * Unit test verifying that <code>0x01 + 0x02 = 0x03</code>.
+     * Builds a vectorial function, adds an input vector, simplifies and asserts that the result equals the expected
+     * output vector.
+     *
+     * @param builder           The builder.
+     * @param inputVectorLength The length of the input vector.
+     * @param inputValue        The input vector as a Long value.
+     * @param outputValue       The output vector as a Long value.
+     */
+    public void buildSimplifyAndAssert(final VectorialFunctionBuilder builder, final int inputVectorLength,
+            final Long inputValue, final Long outputValue) {
+        builder.inputVectorName("i");
+        builder.outputVectorName("o");
+        builder.build();
+        VectorialFunction addFunction = builder.build();
+        Word inputVector = new Word("i", inputVectorLength);
+        ModifiableMap<Variable, Expression> inputVectorMap = ModifiableMap.empty();
+        inputVectorMap.addAll(VectorialFunctionBuilder.buildAssignmentFunctions(inputVector, inputValue));
+        Word outputVector = builder.getOutputVector();
+        VectorialFunction addFunctionWithInputVector = addFunction.extendWih(inputVectorMap);
+        VectorialFunction actual = addFunctionWithInputVector.simplify(outputVector);
+        ModifiableMap<Variable, Expression> expectedMap = ModifiableMap.empty();
+        expectedMap.addAll(VectorialFunctionBuilder.buildAssignmentFunctions(outputVector, outputValue));
+        VectorialFunction expected = new VectorialFunction(expectedMap);
+        assertTrue(expected.containsSame(actual));
+    }
+
+    /**
+     * Unit test verifying that <code>0x01 + 0x02 = 0x03</code>. This unit test uses the subclass
+     * <code>AddFunctionBuilder</code> to test the <code>buildAdditionFunctions</code> method.
      */
     @Test
     public void buildAdditionFunctionsShouldBuildAVectorialFunctionAddingCorrectly() {
         AddFunctionBuilder builder = new AddFunctionBuilder();
         builder.outputVectorWidth(8);
-        builder.inputVectorName("i");
-        builder.outputVectorName("o");
-        builder.build();
-        VectorialFunction addFunction = builder.build();
-        Word inputVector = new Word("i", 16);
-        ModifiableMap<Variable, Expression> inputVectorMap = ModifiableMap.empty();
-        inputVectorMap.addAll(VectorialFunctionBuilder.buildAssignmentFunctions(inputVector, 0x0102L));
-        Word outputVector = builder.getOutputVector();
-        VectorialFunction addFunctionWithInputVector = addFunction.extendWih(inputVectorMap);
-        VectorialFunction actual = addFunctionWithInputVector.simplify(outputVector);
-        ModifiableMap<Variable, Expression> expectedMap = ModifiableMap.empty();
-        expectedMap.addAll(VectorialFunctionBuilder.buildAssignmentFunctions(outputVector, 0x03L));
-        VectorialFunction expected = new VectorialFunction(expectedMap);
-        assertTrue(expected.containsSame(actual));
+        buildSimplifyAndAssert(builder, 16, 0x0102L, 0x03L);
+    }
+
+    /**
+     * Unit test verifying that <code>0x53 & 0x35 = 0x11</code>. This unit test uses the subclass
+     * <code>BasicVectorialFunctionBuilder</code> to test the <code>buildOperationFunctions</code> method.
+     */
+    @Test
+    public void buildOperationFunctionsShouldBuildAVectorialFunctionAndingCorrectly() {
+        BasicVectorialFunctionBuilder builder = new BasicVectorialFunctionBuilder();
+        builder.outputVectorWidth(8);
+        builder.operator(Operator.AND);
+        buildSimplifyAndAssert(builder, 16, 0x5335L, 0x11L);
+    }
+
+    /**
+     * Unit test verifying that <code>0x84 R< 2 = 0x12</code>. This unit test uses the subclass
+     * <code>RotationFunctionBuilder</code> to test the <code>buildRotationFunctions</code> method.
+     */
+    @Test
+    public void buildRotationFunctionsShouldBuildAVectorialFunctionRotatingCorrectly() {
+        RotationFunctionBuilder builder = new RotationFunctionBuilder();
+        builder.outputVectorWidth(8);
+        builder.rotateLeft(2);
+        buildSimplifyAndAssert(builder, 8, 0x84L, 0x12L);
+    }
+
+    /**
+     * Unit test verifying that <code>0xF1 S< 2 = 0xC4</code>. This unit test uses the subclass
+     * <code>ShiftFunctionBuilder</code> to test the <code>buildShiftFunctions</code> method.
+     */
+    @Test
+    public void buildShiftFunctionsShouldBuildAVectorialFunctionShiftingLeftCorrectly() {
+        ShiftFunctionBuilder builder = new ShiftFunctionBuilder();
+        builder.outputVectorWidth(8);
+        builder.shiftLeft(2);
+        buildSimplifyAndAssert(builder, 8, 0xF1L, 0xC4L);
+    }
+
+    /**
+     * Unit test verifying that <code>0x8F S> 2 = 0x23</code>. This unit test uses the subclass
+     * <code>ShiftFunctionBuilder</code> to test the <code>buildShiftFunctions</code> method.
+     */
+    @Test
+    public void buildShiftFunctionsShouldBuildAVectorialFunctionShiftingRightCorrectly() {
+        ShiftFunctionBuilder builder = new ShiftFunctionBuilder();
+        builder.outputVectorWidth(8);
+        builder.shiftRight(2);
+        buildSimplifyAndAssert(builder, 8, 0x8FL, 0x23L);
     }
 }
