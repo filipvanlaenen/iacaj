fac22f7125c931c7365dd08cf3f471926b909b03
Filip Van Laenen <f.a.vanlaenen@ieee.org>
2026-04-03 09:12:45 +0200

    Added unite tests to LiteralExpression

--- src/test/java/net/filipvanlaenen/iacaj/expressions/LiteralExpressionTest.java
+++ src/test/java/net/filipvanlaenen/iacaj/expressions/LiteralExpressionTest.java
@@ -2,13 +2,21 @@ package net.filipvanlaenen.iacaj.expressions;
 
 import static net.filipvanlaenen.iacaj.expressions.Assertions.parseSimplifyAndAssertEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
+
+import net.filipvanlaenen.kolektoj.Map;
 
 /**
  * Unit tests on the <code>LiteralExpression</code> class.
  */
 public class LiteralExpressionTest {
+    /**
+     * The variable a.
+     */
+    private static final Variable VARIABLE_A = new Variable("a");
+
     /**
      * Verifies that <code>true</code> is simplified into <code>true</code>.
      */
@@ -39,5 +47,33 @@ public class LiteralExpressionTest {
     @Test
     public void toStringShouldReturnFalseCorrectly() {
         assertEquals("false", LiteralExpression.FALSE.toString());
+    }
+
+    /**
+     * Verifies a functional test of the simplify as described below.
+     *
+     * <pre>
+     * a = false ⇒ a = false
+     * </pre>
+     */
+    @Test
+    public void simplificationOfLiteralFalse() {
+        VectorialFunction expected = new VectorialFunction(Map.of(VARIABLE_A, LiteralExpression.FALSE));
+        VectorialFunction actual = expected.simplify(new Word(VARIABLE_A));
+        assertTrue(actual.containsSame(expected));
+    }
+
+    /**
+     * Verifies a functional test of the simplify as described below.
+     *
+     * <pre>
+     * a = true ⇒ a = true
+     * </pre>
+     */
+    @Test
+    public void simplificationOfLiteralTrue() {
+        VectorialFunction expected = new VectorialFunction(Map.of(VARIABLE_A, LiteralExpression.TRUE));
+        VectorialFunction actual = expected.simplify(new Word(VARIABLE_A));
+        assertTrue(actual.containsSame(expected));
     }
 }
